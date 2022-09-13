96b188a865f6b3e8341458d68dfd108beee2db2a
Fabio-A-Sa <up202007658@fe.up.pt>
2022-09-13 20:33:20 +0100

    casting

--- Exercises/1 - Basis.hs
+++ Exercises/1 - Basis.hs
@@ -20,5 +20,9 @@ qsort = \list ->
               where left = [x1 | x1 <- xs, x1 <= x]
                     right = [x2 | x2 <- xs, x2 > x]
 
+-- Média de uma lista usando casting
+media :: Fractional a => [a] -> a
+media list = sum list / fromIntegral(length list)
+
 -- main = function to run
-main = print (qsort [5, 2, 3, 4, -1, 0, 6, 200, -3, -3, 23, 5, -2, 0])
+main = print (media [5, 2, 3, 4, -1, 0, 6, 200, -3, -3, 23, 5, -2, 0])
