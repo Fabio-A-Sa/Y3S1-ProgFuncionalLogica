8fb5ae7a9c6efb4652ab750dcefd0c43e5b8caee
Fabio-A-Sa <up202007658@fe.up.pt>
2022-09-13 18:36:48 +0100

    another example

--- Exercises/1 - Basis.hs
+++ Exercises/1 - Basis.hs
@@ -4,6 +4,9 @@ hello_world = print "Hello world!"
 -- A simple function that prints the sum of squares
 anotherFunction = print (sum (map (^2) [1..10]))
 
+-- Composição de funções
+dobro x = 2*x
+quadruplo x = dobro (dobro x)
 
 -- main = function to run
-main = anotherFunction
+main = print (quadruplo 5)
