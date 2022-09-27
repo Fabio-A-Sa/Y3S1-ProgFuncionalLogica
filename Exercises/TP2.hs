a9f8d4b248225314bf5e4ab7d1b23abad0952239
Fabio-A-Sa <up202007658@fe.up.pt>
2022-09-27 09:34:24 +0100

    replicate

--- Exercises/TP2.hs
+++ Exercises/TP2.hs
@@ -6,3 +6,13 @@ myconcat1 (h:t) = h ++ (myconcat1 t)
 
 myconcat2 :: [[a]] -> [a]
 myconcat2 lists = [value | list <- lists , value <- list]
+
+myreplicate1 :: Integral a => a -> b -> [b]
+myreplicate1 0 _ = []
+myreplicate1 n x
+    | n > 0 = x : myreplicate1 (n-1) x
+    | n < 0 = error "myreplicate: n must be positive"
+
+myreplicate2 :: Integral a => a -> b -> [b]
+myreplicate2 n x = [x | _ <- [1..n]]
+
