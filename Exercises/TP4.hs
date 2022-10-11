d6e105a1331a93311f5921bc136cb2d3fdd7965e
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-11 09:26:48 +0100

    pi calculo

--- Exercises/TP4.hs
+++ Exercises/TP4.hs
@@ -8,9 +8,16 @@ sumArv :: (Num a) => Arv a -> a
 sumArv Vazia = 0
 sumArv (No a l r) = (sumArv l) + a + (sumArv r) 
 
--- 4.2
+-- 4.4
 
 mapArv :: (a -> b) -> Arv a -> Arv b
 mapArv function Vazia = Vazia
 mapArv function (No a l r) = No (function a) (mapArv function l) (mapArv function r)
 
+-- Infinit lists
+
+calcPi1 :: (Floating b, Enum b) => Int -> Int -> b 
+calcPi1 1 n = sum [fromIntegral(4*(-1)^i) / fromIntegral(1+2*i) | i <- [0..n]]
+
+calcPi2 :: (Floating b, Enum b) => Int -> Int -> b 
+calcPi2 1 n = 3 + sum [fromIntegral(4*(-1)^i) / fromIntegral(product [2*(i+1) .. 2*(i+1)+2]) | i <- [0..n-1]]
