0cd9c9ebaac8c8fde95cbd22522544c7fb4ca2fb
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-11 09:05:23 +0100

    4.2

--- Exercises/TP4.hs
+++ Exercises/TP4.hs
@@ -2,9 +2,15 @@
 
 -- 4.1
 
-data Arv a = Vazia | No a (Arv a) (Arv a)
+data Arv a = Vazia | No a (Arv a) (Arv a) deriving Show
 
 sumArv :: (Num a) => Arv a -> a
 sumArv Vazia = 0
-sumArv No a l r = (sumArv l) + a + (sumArv r) 
+sumArv (No a l r) = (sumArv l) + a + (sumArv r) 
+
+-- 4.2
+
+mapArv :: (a -> b) -> Arv a -> Arv b
+mapArv function Vazia = Vazia
+mapArv function (No a l r) = No (function a) (mapArv function l) (mapArv function r)
 
