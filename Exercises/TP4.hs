80564ccc5406fa4463b8d7da240f0b9a05c71ec6
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-11 09:53:05 +0100

    drafts

--- Exercises/TP4.hs
+++ Exercises/TP4.hs
@@ -19,5 +19,7 @@ mapArv function (No a l r) = No (function a) (mapArv function l) (mapArv functio
 calcPi1 :: (Floating b, Enum b) => Int -> Int -> b 
 calcPi1 1 n = sum [fromIntegral(4*(-1)^i) / fromIntegral(1+2*i) | i <- [0..n]]
 
+calcPi2 2 n = sum $ take n $ zipWith (/) (cycle [4, -4]) [1 + 2*i | i <- [0 ..]]
+
 calcPi2 :: (Floating b, Enum b) => Int -> Int -> b 
-calcPi2 1 n = 3 + sum [fromIntegral(4*(-1)^i) / fromIntegral(product [2*(i+1) .. 2*(i+1)+2]) | i <- [0..n-1]]
+calcPi2 1 n = 3 + sum [fromIntegral(4*(-1)^i) / fromIntegral(product [(2*(i+1)) .. (2*(i+1)+2)]) | i <- [0..n-1]]
