a32f545520030b985fb03f03a7c8acf7db331ca1
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-18 09:14:25 +0100

    invertinput

--- Exercises/TP5.hs
+++ Exercises/TP5.hs
@@ -2,3 +2,13 @@
 
 invertInput :: IO ()
 invertInput = do 
+    str <- getLine
+    putStrLn $ reverse str
+
+invertInput' :: IO ()
+invertInput' = do 
+    str <- getLine
+    if (null str) then return ()
+    else do
+        putStrLn $ reverse str
+        invertInput'
