36e1e973c4ceb221a44aa96683af959784c8c0e6
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-19 23:18:22 +0100

    1.16

--- Exercises/Fichas/Ficha1.hs
+++ Exercises/Fichas/Ficha1.hs
@@ -167,3 +167,100 @@ mediana a b c = (sort [a, b, c]) !! 1
 mediana' :: (Num a, Ord a) => a -> a -> a -> a
 mediana' a b c = (a+b+c) - min a (min b c) - max a (max b c)
 
+-- 1.16
+
+unidades :: [String]
+unidades =
+  [ "zero"
+  , "um"
+  , "dois"
+  , "tres"
+  , "quatro"
+  , "cinco"
+  , "seis"
+  , "sete"
+  , "oito"
+  , "nove"
+  ]
+
+dez_a_dezanove :: [String]
+dez_a_dezanove =
+  [ "dez"
+  , "onze"
+  , "doze"
+  , "treze"
+  , "quatorze"
+  , "quinze"
+  , "dezasseis"
+  , "dezassete"
+  , "dezoito"
+  , "dezanove"
+  ]
+
+dezenas :: [String]
+dezenas =
+  [ "vinte"
+  , "trinta"
+  , "quarenta"
+  , "cinquenta"
+  , "sessenta"
+  , "setenta"
+  , "oitenta"
+  , "noventa"
+  ]
+
+converte2 :: Int -> String
+converte2 n | n<100 = combina2 (divide2 n)
+
+divide2 :: Int -> (Int, Int)
+divide2 n = (n`div`10, n`mod`10)
+
+combina2 :: (Int, Int) -> String
+combina2 (0, u) = unidades !! u
+combina2 (1, u) = dez_a_dezanove !! u
+combina2 (d, 0) = dezenas !! (d-2)
+combina2 (d, u) = dezenas !! (d-2) ++ " e " ++ unidades !! u
+
+centenas :: [String]
+centenas =
+  [ "cento"
+  , "duzentos"
+  , "trezentos"
+  , "quatrocentos"
+  , "quinhentos"
+  , "seiscentos"
+  , "setecentos"
+  , "oitocentos"
+  , "novecentos"
+  ]
+
+converte3 :: Int -> String
+converte3 n | n<1000 = combina3 (divide3 n)
+
+divide3 :: Int -> (Int, Int)
+divide3 n = (n`div`100, n`mod`100)
+
+combina3 :: (Int, Int) -> String
+combina3 (0, n) = converte2 n
+combina3 (1, 0) = "cem"
+combina3 (c, 0) = centenas !! (c-1)
+combina3 (c, n) = centenas !! (c-1) ++ " e " ++ converte2 n
+
+converte6 :: Int -> String
+converte6 n | n<1000000 = combina6 (divide6 n)
+
+divide6 n = (n `div` 1000, n `mod` 1000)
+
+combina6 (0, n) = converte3 n
+combina6 (1, 0) = "mil"
+combina6 (1, n) = "mil" ++ ligar n ++ converte3 n
+combina6 (m, 0) = converte3 m ++ " mil"
+combina6 (m, n) = converte3 m ++ " mil" ++ ligar n ++ converte3 n
+
+ligar :: Int -> String
+ligar r
+  | r < 100 || r `mod` 100 == 0 = " e "
+  | otherwise                   = " "
+
+converte :: Int -> String
+converte = converte6
