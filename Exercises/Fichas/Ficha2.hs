dc9838a58099bdd74d72c3373ba8b978b023c3a7
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-21 00:20:51 +0100

    2.1

--- Exercises/Fichas/Ficha2.hs
+++ Exercises/Fichas/Ficha2.hs
@@ -2,3 +2,25 @@
 
 -- 2.1
 
+myand :: [Bool] -> Bool
+myand [] = False
+myand (a:b) = a && myand b 
+
+myor :: [Bool] -> Bool
+myor [] = False 
+myor (a:b) = a || myor b
+
+myconcat :: [[a]] -> [a]
+myconcat [] = []
+myconcat (x:xs) = x ++ myconcat xs
+
+myreplicate :: Int -> a -> [a]
+myreplicate i l = [l] ++ myreplicate (i-1) l
+
+(!!!) :: [a] -> Int -> a
+(!!!) list 0 = head list
+(!!!) list n = (!!!) (tail list) (n-1)
+
+myelem :: Eq a => a -> [a] -> Bool
+myelem _ [] = False 
+myelem c (x:xs) = c == x || myelem c xs
