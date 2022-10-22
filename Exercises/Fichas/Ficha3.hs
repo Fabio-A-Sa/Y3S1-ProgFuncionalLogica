f127e0bea3b60facad3894d16d866f931345389f
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-23 00:28:09 +0100

    3.6

--- Exercises/Fichas/Ficha3.hs
+++ Exercises/Fichas/Ficha3.hs
@@ -47,3 +47,8 @@ myfoldl1 function list = foldl (\acc x -> function acc x) (head list) (tail list
 myfoldr1 :: (a -> a -> a) -> [a] -> a 
 myfoldr1 function [] = error "Empty list"
 myfoldr1 function list = foldr (\x acc -> function acc x) (last list) (init list)
+
+-- 3.6
+
+mdc :: Int -> Int -> Int
+mdc a b = fst $ until (\(a, b) -> b == 0) (\(a, b) -> (b, mod a b)) (a, b)
