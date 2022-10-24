ef645a977ad74f5c59c3c546555efd47ee95dbdc
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-25 00:10:17 +0100

    3.3

--- Exercises/Fichas/Ficha3.hs
+++ Exercises/Fichas/Ficha3.hs
@@ -11,3 +11,9 @@ dec2int list = foldl (\acc x -> acc*10+x) 0 list
 
 -- 3.3
 
+recZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+recZipWith function [] [] = []
+recZipWith function a [] = []
+recZipWith function [] b = []
+recZipWith function (x:xs) (y:ys) = [function x y] ++ recZipWith function xs ys 
+
