a9d59d5ccaec74961d704ff4d819e76ae7a48bc3
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-24 14:11:34 +0100

    mPower

--- Exercises/Livro/Capitulo1.hs
+++ Exercises/Livro/Capitulo1.hs
@@ -8,3 +8,9 @@ testpH concentration
     | ph < 7 = "Acid"
     | otherwise = "Basic"
     where ph = -logBase 10 concentration   
+
+-- IN-16
+
+mPower :: (Fractional a, Integral t) => a -> t -> a 
+mPower _ 0 = 1
+mPower x n = x * mPower x (n-1)
