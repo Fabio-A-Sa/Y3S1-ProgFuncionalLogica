fe2a594f12e81d407bcc46e6f3687521c2ed33f6
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-24 14:02:57 +0100

    1

--- Exercises/Livro/Capitulo1.hs
+++ Exercises/Livro/Capitulo1.hs
@@ -1,2 +1,10 @@
 -- 2022/10/25
 
+-- IN-10
+
+testpH :: (Ord a, Floating a) => a -> String
+testpH concentration 
+    | ph == 7 = "Neutral"
+    | ph < 7 = "Acid"
+    | otherwise = "Basic"
+    where ph = -logBase 10 concentration   
