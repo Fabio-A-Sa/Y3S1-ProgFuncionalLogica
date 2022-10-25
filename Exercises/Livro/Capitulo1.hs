bba669c78772e733271dd993fb7265bc734babe3
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-25 14:18:28 +0100

    17, 18

--- Exercises/Livro/Capitulo1.hs
+++ Exercises/Livro/Capitulo1.hs
@@ -14,3 +14,18 @@ testpH concentration
 mPower :: (Fractional a, Integral t) => a -> t -> a 
 mPower _ 0 = 1
 mPower x n = x * mPower x (n-1)
+
+-- IN-17
+
+fib :: (Num a, Ord a, Num p) => a -> p
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+-- IN-18
+
+ack :: (Num a, Ord a) => a -> a -> a
+ack 0 n = n + 1
+ack m n 
+    | n == 0 && m > 0 = ack (m-1) 1
+    | otherwise = ack (m-1) (ack m (n-1))
