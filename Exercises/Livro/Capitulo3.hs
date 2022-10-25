40189797158e976541e9906e01648d8c9c5017ae
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-25 23:29:20 +0100

    LI-23

--- Exercises/Livro/Capitulo3.hs
+++ Exercises/Livro/Capitulo3.hs
@@ -168,4 +168,17 @@ sSort l = m:(sSort (delete m l))
 
 -- LI-23
 
+mSort :: Ord a => [a] -> [a]
+mSort [] = []
+mSort [x] = [x]
+mSort l = merge (mSort l1) (mSort l2)
+    where (l1 ,l2) = splitAt (div (length l) 2) l
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] l = l
+merge l [] = l
+merge (x:xs) (y:ys)
+    | x < y = x:( merge xs (y:ys))
+    | otherwise = y:( merge (x:xs) ys)
+    
 -- LI-24
