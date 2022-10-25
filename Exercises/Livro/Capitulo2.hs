23580f940f02c0f5d2abf78b6fd1db41d0e87037
Fabio-A-Sa <up202007658@fe.up.pt>
2022-10-25 14:42:30 +0100

    FT4

--- Exercises/Livro/Capitulo2.hs
+++ Exercises/Livro/Capitulo2.hs
@@ -14,3 +14,11 @@ mySnd (_, x) = x
 
 mySwap :: (a, b) -> (b, a)
 mySwap (x, y) = (y,x)
+
+-- FT-4
+
+distance2 :: (Ord a, Floating a) => (a, a) -> (a, a) -> a 
+distance2 (x1, y1) (x2, y2) = sqrt $ sum [(x1-x2)^2, (y1-y2)^2]
+
+distanceInf :: (Ord a, Floating a) => (a, a) -> (a, a) -> a 
+distanceInf (x1, y1) (x2, y2) = max (abs (x1-x2)) (abs (y1-y2))
