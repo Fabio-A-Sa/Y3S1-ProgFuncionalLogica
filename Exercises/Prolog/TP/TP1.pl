78cf54659ecd163d92c49be67e7f8a7c5a9a7233
Fabio-A-Sa <up202007658@fe.up.pt>
2022-11-08 09:18:37 +0000

    drafts

---
+++ Exercises/Prolog/TP/TP1.pl
@@ -0,0 +1,72 @@
+% 2022/11/08
+
+% 1.a 
+
+male(frank).
+male(jay).
+male(javier).
+male(merle).
+male(phil).
+male(mitchell).
+male(joe).
+male(pameron).
+male(bo).
+male(dylan).
+male(luke).
+male(rexford).
+male(calhoun).
+male(george).
+
+female(grace).
+female(dede).
+female(gloria).
+female(barb).
+female(claire).
+female(manny).
+female(cameron).
+female(haley).
+female(alex).
+female(lily).
+female(poppy).
+
+parent(grace,phil).
+parent(frank,phil).
+
+parent(dede,claire).
+parent(jay,claire).
+parent(dede,mitchell).
+parent(jay,mitchell).
+
+parent(jay,joe).
+parent(gloria,joe).
+
+parent(gloria,manny).
+parent(javier,manny).
+
+parent(barb,cameron).
+parent(merle,cameron).
+parent(barb,pameron).
+parent(merle,pameron).
+
+parent(phil, haley).
+parent(claire, haley).
+parent(phil, alex).
+parent(claire, alex).
+parent(phil, luke).
+parent(claire, luke).
+
+parent(mitchell, lily).
+parent(mitchell, rexford).
+parent(cameron, lily).
+parent(cameron, rexford).
+
+parent(pameron, calhoun).
+parent(bo, calhoun).
+
+parent(dylan, george).
+parent(dylan, poppy).
+parent(haley, george).
+parent(haley, poppy).
+
+% 1.b 
+
