e323927dbf13aaef9a7d97ad86ff826e1b391a3f
Fabio-A-Sa <up202007658@fe.up.pt>
2022-11-08 09:50:34 +0000

    1.b

--- Exercises/Prolog/TP/TP1.pl
+++ Exercises/Prolog/TP/TP1.pl
@@ -31,38 +31,30 @@ female(poppy).
 
 parent(grace,phil).
 parent(frank,phil).
-
 parent(dede,claire).
 parent(jay,claire).
 parent(dede,mitchell).
 parent(jay,mitchell).
-
 parent(jay,joe).
 parent(gloria,joe).
-
 parent(gloria,manny).
 parent(javier,manny).
-
 parent(barb,cameron).
 parent(merle,cameron).
 parent(barb,pameron).
 parent(merle,pameron).
-
 parent(phil, haley).
 parent(claire, haley).
 parent(phil, alex).
 parent(claire, alex).
 parent(phil, luke).
 parent(claire, luke).
-
 parent(mitchell, lily).
 parent(mitchell, rexford).
 parent(cameron, lily).
 parent(cameron, rexford).
-
 parent(pameron, calhoun).
 parent(bo, calhoun).
-
 parent(dylan, george).
 parent(dylan, poppy).
 parent(haley, george).
@@ -70,8 +62,14 @@ parent(haley, poppy).
 
 % 1.b 
 
-female(haley).              % yes
-male(gil).                  % no
-parent(frank, phil).        % no
-parent(x, claire).          % dede ; jay ; no
-parent(clair, x).           % joe ; manny ; no
+female(haley).                      % yes
+male(gil).                          % no
+parent(frank, phil).                % no
+parent(x, claire).                  % dede ; jay ; no
+parent(clair, X).                   % joe ; manny ; no
+parent(jay, X), parent(X, Y).       % 5 resultados, pares (X, Y), Y = neto
+parent(jay,_X), parent(_X, Y)       % 5 resultados, somente os Y
+parent(X, lily), parent(Y, X).      % 4 resultados, pares (X, Y), Y = avós
+parent(_X, lily), parent(Y, _X).    % 4 resultados, somente os Y
+parent(alex, X).                    % no 
+parent(jay, X), \+parent(gloria, X) % claire ; mitchel ; no
