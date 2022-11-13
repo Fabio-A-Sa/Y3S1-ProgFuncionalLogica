78794a5fae6ac406a2294ae784c7b168a66b8c03
Fabio-A-Sa <up202007658@fe.up.pt>
2022-11-13 14:06:31 +0000

    Exercise 5 database

--- Exercises/Prolog/TP1.pl
+++ Exercises/Prolog/TP1.pl
@@ -295,3 +295,23 @@ legenda(3, 'ID Desconhecido').
 
 % Exercise 5
 
+cargo(tecnico, eleuterio).
+cargo(tecnico, juvenaldo).
+cargo(analista, leonilde).
+cargo(analista, marciliano).
+cargo(engenheiro, osvaldo).
+cargo(engenheiro, porfirio).
+cargo(engenheiro, reginaldo).
+cargo(supervisor, sisnando).
+cargo(supervisor_chefe, gertrudes).
+cargo(secretaria_exec, felismina).
+cargo(diretor, asdrubal).
+chefiado_por(tecnico, engenheiro).
+chefiado_por(engenheiro, supervisor).
+chefiado_por(analista, supervisor).
+chefiado_por(supervisor, supervisor_chefe).
+chefiado_por(supervisor_chefe, diretor).
+chefiado_por(secretaria_exec, diretor).
+
+% 5.a 
+
