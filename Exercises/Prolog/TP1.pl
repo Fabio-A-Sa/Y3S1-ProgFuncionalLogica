91d7660b11da2f43f965accadbac469213aba18a
Fabio-A-Sa <up202007658@fe.up.pt>
2022-11-13 14:44:17 +0000

    7.a

--- Exercises/Prolog/TP1.pl
+++ Exercises/Prolog/TP1.pl
@@ -366,3 +366,26 @@ pairs(X, X):-
 
 % Exercise 7 
 
+% 7.a 
+
+a(a1, 1).
+a(A2, 2).
+a(a3, N).
+b(1, b1).
+b(2, B2).
+b(N, b3).
+
+c(X, Y):- 
+    a(X, Z), 
+    b(Z, Y).
+
+d(X, Y):- 
+    a(X, Z), 
+    b(Y, Z).
+
+d(X, Y):- 
+    a(Z, X), 
+    b(Z, Y).
+
+% 7.b 
+
