18536e659fb788c00f3479770c2944fff8f559fa
Fabio-A-Sa <up202007658@fe.up.pt>
2022-12-13 09:56:19 +0000

    save

--- Exercises/Prolog/TP6.pl
+++ Exercises/Prolog/TP6.pl
@@ -12,3 +12,33 @@
 
 % 5.b
 
+:-op(400, fx, if).
+:-op(500, xfx, then).
+:-op(400, xfx, else).
+
+if X then Y else _ :-
+    call(X), !, call(Y).
+
+if X then _ else Z :-
+    \+call(X), call(Z).
+
+% Exercício extra
+
+% separate (+List, +Pred, -SortedList).
+% SortedList - [Ys, ......, Ns].
+
+separate(L, Pred, SortedList):-
+    separate_aux(L, Pred, Ys, Ns),
+    append(Ys, Ns, SortedList).
+
+separate_aux([], _, [], []).
+separate_aux([H|T], Pred, [H, Ys], Ns):-
+    Goal =.. [Pred, H],
+    call(Goal), !, 
+    separate_aux(T, Pred, Ys, Ns).
+separate_aux([H|T], Pred, Ys, [H,Ns]):-
+    separate_aux(T, Pred, Ys, Ns).
+
+even(X):- 0 =:= X mod 2.
+
+% Exercício extra com otimização
