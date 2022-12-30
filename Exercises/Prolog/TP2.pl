a473efdb655b9e7e93e14341b8ede2bd75c4a849
Fabio-A-Sa <up202007658@fe.up.pt>
2022-12-30 22:35:14 +0000

    1.b

--- Exercises/Prolog/TP2.pl
+++ Exercises/Prolog/TP2.pl
@@ -12,6 +12,15 @@ factorial(N, F):-
     factorial(N1, F1),
     F is F1*N.
 
+% 1.b 
+
+%somaRec(+N, ?Sum)
+somaRec(0, 0).
+somaRec(N, Sum):-
+    N1 is N-1,
+    somaRec(N1, Sum1),
+    Sum is Sum1 + N.
+
 % 1.c
 
 %fibonacci(+N, ?F)
