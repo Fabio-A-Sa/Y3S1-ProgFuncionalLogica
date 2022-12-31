bef34b4504aad75d6f9d1d3f4cc91fb525b1f278
Fabio-A-Sa <up202007658@fe.up.pt>
2022-12-31 22:31:38 +0000

    1.c

--- Exercises/Prolog/TP2.pl
+++ Exercises/Prolog/TP2.pl
@@ -1,5 +1,18 @@
 % 2022/11/15
 
-mya([], L, L).
-mya([H|T], L2, [H|T3]):- 
-    mya(T, L2, T3).
+% Exercício 1
+
+
+
+% 1.c
+
+%fibonacci(+N, ?F)
+fibonacci(0, 0).
+fibonacci(1, 1).
+fibonacci(2, 2).
+fibonacci(N, F):-
+    N1 is N-1,
+    N2 is N-2,
+    fibonacci(N1, F1),
+    fibonacci(N2, F2),
+    F is F1 + F2.
