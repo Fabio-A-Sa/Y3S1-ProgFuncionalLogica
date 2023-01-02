996d5bf2bd9fb934b3042be7081f0aab466ef20b
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-02 21:29:41 +0000

    1.a.b.c

--- Exercises/Prolog/TP3.pl
+++ Exercises/Prolog/TP3.pl
@@ -1,33 +1,12 @@
 % 2022/11/22
 
-% Ficha 2
+% Exercício 1
 
-invert([], []).
-invert([X|Xs], Rev):-
-    invert(Xs, Rev1),
-    append(Rev1, [X], Rev).
+s(1).
+s(2):- !.
+s(3).
 
-invert2(L, Rev):-
-    invert2_aux(L, [], Rev).
+% a) X = 1, X = 2
+% b) X = 1, Y = 1 ; X = 1, Y = 2 ; X = 2, Y = 1 ; X = 2, Y = 1
+% c) X = 1, Y = 1 ; X = 1, Y = 2
 
-invert2_aux([], Acc, Acc).
-invert2_aux([X|Xs], Acc, Rev):-
-    invert2_aux(Xs,[X, Acc], Rev).
-
-list_member(Element, List).
-    append(_, [Element, _], List).
-
-list_nth(Index, List, Element):-
-    append(Prefix, [Element,_], List),
-    length(Prefix, N).
-
-% Ficha 3
-
-course_classes(UC, Course):-
-    findall(Dia/Hora-Tipo, class(UC, Tipo, Dia, Hora, _), L).
-
-short_classes(L):-
-    findall(UC-Dia/Hora, (class(UC, _, Dia, Hora, Dur), Dur < 2), L).
-
-same_day(UC1, UC2):-
-    findall(D, (class(UC1, _, D, _, _), class(UC2, _, D, _, _)), [_|_]).
