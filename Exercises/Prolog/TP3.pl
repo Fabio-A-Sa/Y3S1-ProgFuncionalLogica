77715e8b64c544f77919c2686b765933c883ea4f
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-02 17:08:22 +0000

    database for exercise 5

--- Exercises/Prolog/TP3.pl
+++ Exercises/Prolog/TP3.pl
@@ -158,3 +158,66 @@ tree(N, Max):-
     print_n(' ', Padding), print_n('*', Stars), nl,
     NextTree is N + 1,
     tree(NextTree, Max).
+
+% Exercise 5
+
+male(frank).
+male(jay).
+male(javier).
+male(merle).
+male(phil).
+male(mitchell).
+male(joe).
+male(pameron).
+male(bo).
+male(dylan).
+male(luke).
+male(rexford).
+male(calhoun).
+male(george).
+
+female(grace).
+female(dede).
+female(gloria).
+female(barb).
+female(claire).
+female(manny).
+female(cameron).
+female(haley).
+female(alex).
+female(lily).
+female(poppy).
+
+parent(grace,phil).
+parent(frank,phil).
+parent(dede,claire).
+parent(jay,claire).
+parent(dede,mitchell).
+parent(jay,mitchell).
+parent(jay,joe).
+parent(gloria,joe).
+parent(gloria,manny).
+parent(javier,manny).
+parent(barb,cameron).
+parent(merle,cameron).
+parent(barb,pameron).
+parent(merle,pameron).
+parent(phil, haley).
+parent(claire, haley).
+parent(phil, alex).
+parent(claire, alex).
+parent(phil, luke).
+parent(claire, luke).
+parent(mitchell, lily).
+parent(mitchell, rexford).
+parent(cameron, lily).
+parent(cameron, rexford).
+parent(pameron, calhoun).
+parent(bo, calhoun).
+parent(dylan, george).
+parent(dylan, poppy).
+parent(haley, george).
+parent(haley, poppy).
+
+% 5.a
+
