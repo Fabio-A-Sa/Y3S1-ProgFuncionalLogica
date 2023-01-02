ce80235d89aea00e56673001816d1f662c74cf24
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-02 14:41:40 +0000

    exercise 2

--- Exercises/Prolog/TP3.pl
+++ Exercises/Prolog/TP3.pl
@@ -10,3 +10,15 @@ s(3).
 % b) X = 1, Y = 1 ; X = 1, Y = 2 ; X = 2, Y = 1 ; X = 2, Y = 1
 % c) X = 1, Y = 1 ; X = 1, Y = 2
 
+% Exercício 2
+
+data(one).
+data(two).
+data(three).
+cut_test_a(X):- data(X).
+cut_test_a('five').
+cut_test_b(X):- data(X), !.
+cut_test_b('five').
+cut_test_c(X, Y):- data(X), !, data(Y).
+cut_test_c('five', 'five').
+
