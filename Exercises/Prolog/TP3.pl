93478aecde4ac3fb87cc84e95de4b77a54f4982b
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-03 18:31:27 +0000

    added exercise 7 database

--- Exercises/Prolog/TP3.pl
+++ Exercises/Prolog/TP3.pl
@@ -350,3 +350,23 @@ strangers(L):-
 %good_groups(-L)
 good_groups(L) :-
     setof(S1-S2, (UC1, UC2, UCs, Len)^(frequenta(S1, UC1), frequenta(S2, UC2), common_courses(S1, S2, UCs), S1 @< S2, length(UCs, Len), Len > 1), L).
+
+% Exercício 7
+
+%class(Course, ClassType, DayOfWeek, Time, Duration)
+class(pfl, t, '1 Seg', 11, 1).
+class(pfl, t, '4 Qui', 10, 1).
+class(pfl, tp, '2 Ter', 10.5, 2).
+class(lbaw, t, '1 Seg', 8, 2).
+class(lbaw, tp, '3 Qua', 10.5, 2).
+class(ltw, t, '1 Seg', 10, 1).
+class(ltw, t, '4 Qui', 11, 1).
+class(ltw, tp, '5 Sex', 8.5, 2).
+class(fsi, t, '1 Seg', 12, 1).
+class(fsi, t, '4 Qui', 12, 1).
+class(fsi, tp, '3 Qua', 8.5, 2).
+class(rc, t, '4 Qui', 8, 2).
+class(rc, tp, '5 Sex', 10.5, 2).
+
+% 7.a
+
