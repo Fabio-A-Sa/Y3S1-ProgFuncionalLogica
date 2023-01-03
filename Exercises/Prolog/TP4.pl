41d41458d25837b8d6b475d2375878d8470cde3f
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-03 19:55:02 +0000

    1.a

--- Exercises/Prolog/TP4.pl
+++ Exercises/Prolog/TP4.pl
@@ -1,2 +1,17 @@
 % 2022/11/29
 
+% Exercicio 1
+
+% 1.a
+
+%add_person/0
+add_person:-
+    write('male or female? '),
+    read(male), !,
+    write('name? '),
+    read(Name),
+    asserta(male(Name)).
+add_person:-
+    write('name? '),
+    read(Name),
+    asserta(female(Name)). 
