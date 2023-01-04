2f88c51d3157c1ad7a269b364c4c6f3c3c6846eb
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-04 19:24:31 +0000

    some modifications

--- Notes/Prolog/playing.pl
+++ Notes/Prolog/playing.pl
@@ -14,3 +14,18 @@ is_connected(Origem, Destino):-
 is_connected(Origem, Destino):-
     connected(Origem, Meio),
     is_connected(Meio, Destino).
+
+% Verifica se há conexão entre um ponto e outro, evitando ciclos e mostrando o caminho percorrido.
+
+path(Origem, Destino):-
+    construct_path(Origem, Destino, [Origem], Path),
+    write(Path).
+
+construct_path(Origem, Destino, Acc, Path):-
+    connected(Origem, Destino),
+    append(Acc, [Destino], Path).
+construct_path(Origem, Destino, Acc, Path):-
+    connected(Origem, Meio),
+    \+member(Meio, Acc),
+    append(Acc, [Meio], Acc1),
+    construct_path(Meio, Destino, Acc1, Path).
