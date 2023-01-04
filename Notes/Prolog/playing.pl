a770c2da474c91937196f67bed65517d6e58706d
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-04 19:36:29 +0000

    database schema

--- Notes/Prolog/playing.pl
+++ Notes/Prolog/playing.pl
@@ -1,11 +1,33 @@
-connected(porto, lisbon).
-connected(lisbon, madrid).
-connected(lisbon, paris).
-connected(lisbon, porto).
-connected(madrid, paris).
-connected(madrid, lisbon).
-connected(paris, madrid).
-connected(paris, lisbon).
+connected(a, b).
+connected(b, c).
+connected(f, e).
+connected(c, f).
+connected(e, g).
+connected(k, l).
+connected(m, o).
+connected(r, a).
+connected(s, e).
+connected(t, y).
+connected(q, a).
+connected(p, o).
+connected(f, y).
+connected(t, w).
+connected(t, f).
+connected(f, d).
+connected(d, g).
+connected(d, e).
+connected(e, g).
+connected(e, j).
+connected(j, k).
+connected(l, p).
+connected(q, e).
+connected(q, r).
+connected(e, d).
+connected(f, s).
+connected(d, z).
+connected(z, d).
+connected(s, z).
+connected(a, s).
 
 % Verifica se há conexão entre um ponto e outro. Não verifica ciclos.
 
