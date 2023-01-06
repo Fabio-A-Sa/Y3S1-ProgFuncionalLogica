734e716247d41da258e8bbf9f05f08a4f4d69443
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-06 14:58:01 +0000

    1.b

--- Exercises/Prolog/TP5.pl
+++ Exercises/Prolog/TP5.pl
@@ -2,6 +2,8 @@
 
 % Exercício 1
 
+% 1.a
+
 %map(+Pred, +List1, ?List2)
 map(Predicate, List, Result):-
     map_aux(Predicate, List, [], Result).
@@ -15,3 +17,13 @@ map_aux(Predicate, [Element|Rest], Accumulator, Result):-
 
 double(X, Y):- Y is X * 2.
 
+% 1.b
+
+%fold(+Pred, +StartValue, +List, ?FinalValue)
+fold(_, Value, [], Value).
+fold(Pred, StartValue, [Element|Rest], FinalValue):-
+    Predicate =.. [Pred, StartValue, Element, Result],
+    Predicate,
+    fold(Pred, Result, Rest, FinalValue).
+
+sum(A, B, R):- R is A + B.
