ab110c921f3862f7ff68acfb902e35b118baf8cb
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-06 14:53:04 +0000

    1.a

--- Exercises/Prolog/TP5.pl
+++ Exercises/Prolog/TP5.pl
@@ -2,3 +2,16 @@
 
 % Exercício 1
 
+%map(+Pred, +List1, ?List2)
+map(Predicate, List, Result):-
+    map_aux(Predicate, List, [], Result).
+
+map_aux(_, [], Result, Result).
+map_aux(Predicate, [Element|Rest], Accumulator, Result):-
+    Operation =.. [Predicate, Element, R],
+    Operation, 
+    append(Accumulator, [R], Accumulator1),
+    map_aux(Predicate, Rest, Accumulator1, Result).
+
+double(X, Y):- Y is X * 2.
+
