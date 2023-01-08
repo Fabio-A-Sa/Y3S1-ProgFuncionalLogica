0d80aed9a36deaa4d7f5aaae58ba25b0e7c39b53
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-08 00:46:59 +0000

    database

--- Exercises/Prolog/Exercises.pl
+++ Exercises/Prolog/Exercises.pl
@@ -8,3 +8,24 @@
 :- use_module(library(lists)).
 :- use_module(library(between)).
 
+%player(Name, Username, Age)
+player('Danny', 'Best Player Ever', 27).
+player('Annie', 'Worst Player Ever', 24).
+player('Harry', 'A-Star Player', 26).
+player('Manny', 'The Player', 14).
+player('Johnny', 'A Player', 16).
+
+%game(Name, Categories, MinAge)
+game('5 ATG', [action, adventure, open-world, multiplayer], 18).
+game('Carrier Shift: Game Over', [action, fps, multiplayer, shooter], 16).
+game('Duas Botas', [action, free, strategy, moba], 12).
+
+%played(Player, Game, HoursPlayed, PercentUnlocked)
+played('Best Player Ever', '5 ATG', 3, 83).
+played('Worst Player Ever', '5 ATG', 52, 9).
+played('The Player', 'Carrier Shift: Game Over', 44, 22).
+played('A Player', 'Carrier Shift: Game Over', 48, 24).
+played('A Star Player', 'Duas Botas', 37, 16).
+played('Best Player Ever', 'Duas Botas', 33, 22).
+
+
