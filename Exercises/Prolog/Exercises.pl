84d1dbd24e60712e18b17a2dfefe667133f85e92
Fabio-A-Sa <up202007658@fe.up.pt>
2023-01-12 22:50:50 +0000

    database

--- Exercises/Prolog/Exercises.pl
+++ Exercises/Prolog/Exercises.pl
@@ -749,3 +749,20 @@ my_delete(Element, [AnotherElement|Resto], Result):-
 
 % Teste de Modelo - Mesmo teste de 21/22
 
+:- dynamic round/4.
+
+% round(RoundNumber, DanceStyle, Minutes, [Dancer1-Dancer2 | DancerPairs])
+% round/4 indica, para cada ronda, o estilo de dança, a sua duração, e os pares de dançarinos participantes.
+round(1, waltz, 8, [eugene-fernanda]).
+round(2, quickstep, 4, [asdrubal-bruna,cathy-dennis,eugene-fernanda]).
+round(3, foxtrot, 6, [bruna-dennis,eugene-fernanda]).
+round(4, samba, 4, [cathy-asdrubal,bruna-dennis,eugene-fernanda]).
+round(5, rhumba, 5, [bruna-asdrubal,eugene-fernanda]).
+
+% tempo(DanceStyle, Speed).
+% tempo/2 indica a velocidade de cada estilo de dança.
+tempo(waltz, slow).
+tempo(quickstep, fast).
+tempo(foxtrot, slow).
+tempo(samba, fast).
+tempo(rhumba, slow).
